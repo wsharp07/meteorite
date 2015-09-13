a50a4b8fe1369ea4a1dcf65fe8ec2674d121447a
William Sharp <wsharp@ltu.edu>
2015-09-12 21:01:06 -0400

    fleshing out product show, implementing markdown

--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
 UI.registerHelper("money", function(amount){
 	return accounting.formatMoney(amount);
 });
+UI.registerHelper("markdown", function(text){
+	var converter = new showdown.Converter();
+	return converter.makeHtml(text);
+});
